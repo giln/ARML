22eecca435e25bd33ef4ed1185063f2083665857
Gil Nakache <gil.nakache@viseo.com>
2019-01-28 15:38:21 +0100

    Working but ugly

--- ARML/Classes/ViewController.swift
+++ ARML/Classes/ViewController.swift
@@ -19,8 +19,30 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     let previewView = UIImageView()
     var currentBuffer: CVPixelBuffer?
     let redView = UIView()
+    var previousNode: SCNNode?
+
+    let touchNode : SCNNode = {
+        // Touch node configuration
+        let sphere = SCNSphere(radius: 0.02)
+
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+
+        sphere.firstMaterial = material
+
+        let sphereNode = SCNNode(geometry: nil)
+        //sphereNode.isHidden = true
+
+        let sphereShape = SCNPhysicsShape(geometry: sphere, options: nil)
+
+        let physicsBody = SCNPhysicsBody(type: .kinematic, shape: sphereShape)
+        sphereNode.physicsBody = physicsBody
+
+        return sphereNode
+    }()
 
     let visionQueue = DispatchQueue(label: "com.viseo.ARML.visionqueue")
+
 
     lazy var predictionRequest: VNCoreMLRequest = {
         // Load the ML model through its generated class and create a Vision request for it.
@@ -45,6 +67,9 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
         sceneView.addSubview(redView)
 
+        sceneView.automaticallyUpdatesLighting = false
+        sceneView.autoenablesDefaultLighting = false
+
         redView.backgroundColor = UIColor.red
         redView.translatesAutoresizingMaskIntoConstraints = false
         redView.widthAnchor.constraint(equalToConstant: 10).isActive = true
@@ -54,7 +79,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         previewView.widthAnchor.constraint(equalToConstant: 112).isActive = true
         previewView.heightAnchor.constraint(equalToConstant: 112).isActive = true
 
-        previewView.backgroundColor = UIColor.white
+        previewView.backgroundColor = UIColor.black
         previewView.bottomAnchor.constraint(equalTo: sceneView.bottomAnchor).isActive = true
     }
 
@@ -72,6 +97,14 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
         // Set the scene to the view
         sceneView.scene = scene
+
+        sceneView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(viewDidTap(recognizer:))))
+
+
+        sceneView.scene.rootNode.addChildNode(touchNode)
+
+        insertSpotLight(position: SCNVector3(10, 50, 0))
+
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -79,6 +112,9 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+
+        configuration.planeDetection = .horizontal
+        configuration.environmentTexturing = .automatic
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -91,6 +127,63 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
         // Pause the view's session
         sceneView.session.pause()
+
+    }
+
+    // MARK: - Actions
+
+    func insertSpotLight(position: SCNVector3) {
+        let spotLight = SCNLight()
+        spotLight.type = .directional
+        spotLight.shadowMode = .deferred
+        spotLight.castsShadow = true
+        spotLight.shadowRadius = 5.0
+        // spotLight.categoryBitMask = 4
+        spotLight.shadowColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.2)
+
+        let spotNode = SCNNode()
+        spotNode.light = spotLight
+        spotNode.position = position
+        spotNode.eulerAngles = SCNVector3(-Float.pi / 2, 0, 0)
+        sceneView.scene.rootNode.addChildNode(spotNode)
+    }
+
+    @objc func viewDidTap(recognizer: UITapGestureRecognizer) {
+
+        previousNode?.removeFromParentNode()
+
+        let sphere = SCNSphere(radius: 0.05)
+
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+
+        let reflectiveMaterial = SCNMaterial()
+        reflectiveMaterial.lightingModel = .physicallyBased
+        reflectiveMaterial.metalness.contents = 0.8
+        reflectiveMaterial.roughness.contents = 0.4
+        sphere.firstMaterial = reflectiveMaterial
+
+        //sphere.materials = [material]
+
+        let sphereNode = SCNNode(geometry: sphere)
+
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+
+        guard let hitTestResult = hitTestResults.first else { return }
+
+        let physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        sphereNode.physicsBody = physicsBody
+        //rocketshipNode.name = rocketshipNodeName
+
+        sphereNode.simdTransform = hitTestResult.worldTransform
+
+        sphereNode.position.y += 0.5
+
+        sphereNode.castsShadow = true
+        sceneView.scene.rootNode.addChildNode(sphereNode)
+
+        previousNode = sphereNode
     }
 
     // MARK: - ARSCNViewDelegate
@@ -103,6 +196,69 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
      return node
      }
      */
+
+    func renderer(_: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let plane = SCNPlane(width: width, height: height)
+
+        // 3
+
+        let material = SCNMaterial()
+        material.lightingModel = .constant
+        //material.readsFromDepthBuffer = true
+        material.writesToDepthBuffer = true
+        material.colorBufferWriteMask = []
+
+        plane.firstMaterial = material
+
+        // 4
+        var planeNode = SCNNode(geometry: plane)
+        planeNode.castsShadow = false
+
+        // 5
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+        planeNode.eulerAngles.x = -.pi / 2
+
+        update(&planeNode, withGeometry: plane, type: .static)
+        // 6
+        node.addChildNode(planeNode)
+    }
+
+    func renderer(_: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // 1
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+            var planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+        else { return }
+
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+
+        // 3
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+
+        update(&planeNode, withGeometry: plane, type: .static)
+
+    }
+
+    func update(_ node: inout SCNNode, withGeometry geometry: SCNGeometry, type: SCNPhysicsBodyType) {
+        let shape = SCNPhysicsShape(geometry: geometry, options: nil)
+        let physicsBody = SCNPhysicsBody(type: type, shape: shape)
+        node.physicsBody = physicsBody
+    }
 
     func session(_: ARSession, didFailWithError _: Error) {
         // Present an error message to the user
@@ -139,17 +295,25 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
                 fatalError("unexpected result type from VNCoreMLRequest")
             }
 
-            //let previewImage = UIImage(pixelBuffer: observation.pixelBuffer)
+            let previewImage = UIImage(pixelBuffer: observation.pixelBuffer)
             let foundPoint = observation.pixelBuffer.search()
 
             DispatchQueue.main.async {
-                //self.previewView.image = previewImage
+                self.previewView.image = previewImage
                 self.currentBuffer = nil
 
                 if let point = foundPoint {
                     let imagePoint = VNImagePointForNormalizedPoint(point, Int(self.view.bounds.size.width), Int(self.view.bounds.size.height))
-                    self.redView.isHidden = false
+                    self.redView.isHidden = true
                     self.redView.center = imagePoint
+
+                    let hitTestResults = self.sceneView.hitTest(imagePoint, types: .existingPlaneUsingExtent)
+
+                    guard let hitTestResult = hitTestResults.first else { return }
+
+                    self.touchNode.simdTransform = hitTestResult.worldTransform
+
+
                 } else {
                     self.redView.isHidden = true
                 }
